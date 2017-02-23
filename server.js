057bbdc377d0434a494af448b7ab3eadd8b00014
Abdulah Hamzic <abdulahhamzic@gmail.com>
2017-02-23 11:28:17 +0000

    Fully functional version

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res) {
-    res.send('Hello, world!');
+app.get('/:param', function(req, res) {
+    var object = {},
+    param = req.params.param == Number(req.params.param) ? Number(req.params.param) * 1000 : req.params.param,
+    date = new Date(param),
+    options = {year: 'numeric', month: 'long', day: 'numeric'};
+    object.unix = date.getTime() / 1000;
+    object.natural = date.toLocaleString() !== "Invalid Date" ? date.toLocaleString('en-US', options) : null;
+    res.send(object);
 });
 
 app.listen(8080, function() {
